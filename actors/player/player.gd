351a02f9a4bb1a9f831b9a9586f74e687e1496ee
Lefl1 <lefl65@gmail.com>
2019-03-12 00:05:57 +0100

    Fix lunging out of crew

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 const SPEED = 75
-var is_input_blocked
+var is_input_blocked = false
 
 var is_lunging = false
 var lunge_dir = Vector2()
@@ -27,16 +27,20 @@ func _physics_process(delta):
 
 
 
-func lunge():
+func lunge(dir = null):
 	if lunge_cooldown < LUNGE_COOLDOWN_TIME:
 		return
-	is_input_blocked = true
+	lunge_time = 0
 	is_lunging = true
-	lunge_dir = get_input_dir()
+	if not dir == null:
+		lunge_dir = dir
+	else:
+		lunge_dir = get_input_dir()
+	is_input_blocked = true
 
 
 func lunge_move(delta):
-	if lunge_dir == Vector2.ZERO:
+	if not lunge_dir or lunge_dir == Vector2.ZERO:
 		stop_lunge()
 		return
 	var vel = lunge_dir * LUNGE_SPEED * delta
@@ -45,25 +49,30 @@ func lunge_move(delta):
 		stop_lunge()
 		if col.collider.is_in_group("crew"):
 			host = col.collider
+			host.status = host.POSSESSED
 			get_node("col").set_disabled(true)
 			set_visible(false)
 			set_global_transform(host.get_global_transform())
 
 
 func stop_lunge():
-	is_input_blocked = false
+	print("stopped")
 	is_lunging = false
 	lunge_time = 0
 	lunge_cooldown = 0
 	set_collision_mask_bit(1, true)
+	is_input_blocked = false
 
 
 func release_host():
-	lunge()
+	lunge_cooldown = LUNGE_COOLDOWN_TIME
 	set_collision_mask_bit(1, false)
 	var htform = host.get_global_transform()
 	set_global_transform(htform)
-	lunge_dir = htform.y
+	lunge(htform.y)
+	print("STARTED TO LUNGE")
+	print(is_lunging)
+	host.status = host.STUNNED
 	host = null
 	get_node("col").set_disabled(false)
 	set_visible(true)
@@ -72,13 +81,12 @@ func release_host():
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.scancode == KEY_SPACE:
-			print(is_behind_crew())
-			if host and is_behind_crew():
+			if host and is_behind_crew() and not is_input_blocked:
 				print("Stabby time")
 				host.get_node("front").get_overlapping_bodies()[0].die()
-			elif host:
+			elif host and not is_input_blocked:
 				release_host()
-			else:
+			elif not is_input_blocked:
 				lunge()
 
 
@@ -92,6 +100,7 @@ func is_behind_crew():
 		if body.is_in_group("crew"):
 			if not body.is_dead():
 				return true
+	return false
 
 func move(delta):
 	var dir = get_input_dir()
@@ -103,6 +112,9 @@ func move(delta):
 
 
 func get_input_dir():
+	if is_input_blocked:
+		return
+
 	var dir = Vector2()
 	if Input.is_key_pressed(KEY_A):
 		dir.x += -1
@@ -112,4 +124,4 @@ func get_input_dir():
 		dir.y += -1
 	if Input.is_key_pressed(KEY_S):
 		dir.y += 1
-	return dir	
+	return dir
