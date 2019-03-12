a861d9eb34ed290a2277d9fac777bbcac5f597b4
Lefl1 <lefl65@gmail.com>
2019-03-12 16:35:26 +0100

    Readjust collision masks

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -3,6 +3,15 @@ extends KinematicBody2D
 const SPEED = 75
 var is_input_blocked = false
 
+enum {NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST}
+const MAX_CARDDIR = 7
+const directions_dict = {
+	NORTH: 0, NORTHEAST: 45,
+	EAST: 90, SOUTHEAST: 135,
+	SOUTH: 180, SOUTHWEST: 225,
+	WEST: 270, NORTHWEST: 315}
+const cardinal_margin = 0.2
+
 var is_lunging = false
 var lunge_dir = Vector2()
 const LUNGE_SPEED = 200
@@ -46,10 +55,11 @@ func lunge_move(delta):
 	var vel = lunge_dir * LUNGE_SPEED * delta
 	var col = move_and_collide(vel)
 	if col:
+		print("hit something")
 		stop_lunge()
 		if col.collider.is_in_group("crew"):
 			host = col.collider
-			host.status = host.POSSESSED
+			host.change_state(host.POSSESSED)
 			get_node("col").set_disabled(true)
 			set_visible(false)
 			set_global_transform(host.get_global_transform())
@@ -60,19 +70,19 @@ func stop_lunge():
 	is_lunging = false
 	lunge_time = 0
 	lunge_cooldown = 0
-	set_collision_mask_bit(1, true)
+	set_collision_mask_bit(2, true)
 	is_input_blocked = false
 
 
 func release_host():
 	lunge_cooldown = LUNGE_COOLDOWN_TIME
-	set_collision_mask_bit(1, false)
+	set_collision_mask_bit(2, false)
 	var htform = host.get_global_transform()
 	set_global_transform(htform)
-	lunge(htform.y)
+	lunge()
 	print("STARTED TO LUNGE")
 	print(is_lunging)
-	host.status = host.STUNNED
+	host.change_state(host.STUNNED)
 	host = null
 	get_node("col").set_disabled(false)
 	set_visible(true)
@@ -102,14 +112,44 @@ func is_behind_crew():
 				return true
 	return false
 
+
 func move(delta):
 	var dir = get_input_dir()
 
 	if host:
+		if dir != Vector2.ZERO:
+			host.rotate_to_vdir(dir)
 		host.move_and_slide(dir.normalized() * SPEED)
 	else:
+		# Also rotate!
 		move_and_slide(dir.normalized() * SPEED)
 
+
+func get_rotation_from_dir(dir):
+	var cardinal_margin = 0.2
+	var direction = 0
+
+	if dir.y > 0 and dir.y > cardinal_margin:
+		if dir.x < cardinal_margin and dir.x > -cardinal_margin:
+			direction = SOUTH
+		elif dir.x > cardinal_margin:
+			direction = SOUTHEAST
+		elif dir.x < -cardinal_margin:
+			direction = SOUTHWEST
+
+	elif dir.y < 0 and dir.y < -cardinal_margin:
+		if dir.x < cardinal_margin and dir.x > -cardinal_margin:
+			direction = NORTH
+		elif dir.x > cardinal_margin:
+			direction = NORTHEAST
+		elif dir.x < -cardinal_margin:
+			direction = NORTHWEST
+	elif dir.x > 0:
+		direction = EAST
+	elif dir.x < 0:
+		direction = WEST
+
+	return direction
 
 func get_input_dir():
 	if is_input_blocked:
@@ -124,4 +164,4 @@ func get_input_dir():
 		dir.y += -1
 	if Input.is_key_pressed(KEY_S):
 		dir.y += 1
-	return dir
+	return dir.normalized()
